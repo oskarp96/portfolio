23891d9ee57622223ab4fb8b4ebaf93cb032458e
Oskar Persson <trummsocken@hotmail.com>
2020-04-09 13:46:44 +0200

    Updates

---
+++ precache-manifest.e38e5395a02da9125ce0774d79fa6cc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016b1706e809164aa16dcf011030f2ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a83093a647ce5d09e4b9",
+    "url": "/portfolio/static/css/main.ace93ff6.chunk.css"
+  },
+  {
+    "revision": "0d6e3c4c64d778fa4ba5",
+    "url": "/portfolio/static/js/2.4a4fe698.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.4a4fe698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83093a647ce5d09e4b9",
+    "url": "/portfolio/static/js/main.21c91c92.chunk.js"
+  },
+  {
+    "revision": "11c08c59310c4dc2cf84",
+    "url": "/portfolio/static/js/runtime-main.77956853.js"
+  },
+  {
+    "revision": "25f51426ef58b690eb3feb7e1406b434",
+    "url": "/portfolio/static/media/coding.25f51426.jpg"
+  }
+]);
