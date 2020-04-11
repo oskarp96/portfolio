6520c76b2fb7062613df9480c94ec2f98ccda73b
oskii96 <trummsocken@hotmail.com>
2020-04-11 17:32:55 +0200

    Padding and fa-icons

--- src/About.js
+++ src/About.js
@@ -1,47 +1,58 @@
-import React, { Component } from 'react';
-import Skillbars from '../src/components/Skillbars';
+import React, { Component } from "react";
+import Skillbars from "../src/components/Skillbars";
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div>
-                <div>
-                    <h2>About me</h2>
-                    <hr />
-                    <p>I'm a 24 year old informatics student at Örebro University. By the summer of 2020
-                    I'm graduating with a bachelor's degree in Information Systems. Some of the courses
-                    I've had during my education have dealt with:
-                </p>
-                    <ul>
-                        <li>Object-oriented programming with C#</li>
-                        <li>Object-oriented programming with Java</li>
-                        <li>Database management with SQL Server</li>
-                        <li>Client-side programming with JavaScript, jQuery, HTML and CSS</li>
-                        <li>Web-systems with .NET</li>
-                        <li>Agile software development with Scrum and eXtreme Programming</li>
-                        <li>Requirements engineering</li>
-                        <li>Object-oriented analysis and design</li>
-                        <li>Interaction design</li>
-                    </ul>
-                    <p>... amongst others.</p>
-                </div>
-                <div>
-                    <h2>Projects</h2>
-                    <hr />
-                    <p>You can find my projects over at <a href="https://github.com/oskarp96?tab=repositories">my github.</a></p>
-                </div>
-                <div>
-                    <h2>Skills</h2>
-                    <hr />
-                    <Skillbars />
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="padding">
+        <div className="padding">
+          <h2>About me</h2>
+          <hr />
+          <p>
+            I'm a 24 year old informatics student at Örebro University. By the
+            summer of 2020 I'm graduating with a bachelor's degree in
+            Information Systems. Some of the courses I've had during my
+            education have dealt with:
+          </p>
+          <ul>
+            <li>Object-oriented programming with C#</li>
+            <li>Object-oriented programming with Java</li>
+            <li>Database management with SQL Server</li>
+            <li>
+              Client-side programming with JavaScript, jQuery, HTML and CSS
+            </li>
+            <li>Web-systems with .NET</li>
+            <li>
+              Agile software development with Scrum and eXtreme Programming
+            </li>
+            <li>Requirements engineering</li>
+            <li>Object-oriented analysis and design</li>
+            <li>Interaction design</li>
+          </ul>
+          <p>... amongst others.</p>
+        </div>
+        <div className="padding">
+          <h2>Projects</h2>
+          <hr />
+          <p>
+            You can find my projects over at{" "}
+            <a href="https://github.com/oskarp96?tab=repositories">
+              my github.
+            </a>
+          </p>
+        </div>
+        <div>
+          <h2>Skills</h2>
+          <hr />
+          <Skillbars />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/Contact.js
+++ src/Contact.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div>
-                <h2>Contact</h2>
-                <hr />
-                <address>
-                    <uli>
-                        <li>
-                        <a href="mailto:oskar.persson1996@gmail.com">Mail</a>
-                        </li>
-                        <li>
-                        <a href="https://www.linkedin.com/in/perssonoskar/">LinkedIn</a>
-                        </li>
-                    </uli>
-                </address>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <hr />
+        <address>
+          <a href="mailto:oskar.persson1996@gmail.com">
+            <i
+              class="fas fa-envelope fa-2x"
+              style={{ color: "#222", marginRight: "10px" }}
+            ></i>
+          </a>
+          <a href="https://www.linkedin.com/in/perssonoskar/" target="_blank">
+            <i
+              class="fab fa-linkedin-in fa-2x"
+              style={{ color: "#222", marginRight: "10px" }}
+            ></i>
+          </a>
+        </address>
+      </div>
+    );
+  }
 }
 
 export default Contact;
